965f6708ce6b162a9cc37b227dc73e1bff66d0ad
DCU <bestcar137@gmail.com>
2025-03-18 16:58:47 +0900

    D2_559

---
+++ HW_3_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HW_3_2 {
+    public static void main(String[] args) {
+        int line;
+        Scanner input = new Scanner(System.in);
+
+        line = input.nextInt();
+
+        for(int i=line-1; i>=0; i--) {
+            for(int j=0; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ HW_3_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HW_3_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+
+        while (c <= 'a') ;
+        {
+            c = (char) (c - 1);
+            System.out.print(c);
+
+        }
+    }
+}
