205e237c6fb116c227e4f29e6495a65b2ceae6d4
bestcar137 <bestcar137@gmail.com>
2025-04-02 14:04:39 +0900

    hmm

--- HW5_3_1.java
+++ HW5_3_1.java
@@ -1,44 +1,13 @@
-/*
-public class HW5_3 {
+public class HW5_3_1 {
     public static void main(String[] args) {
         ColorPoint zeroPoint = new ColorPoint(); // BLACK 색에 (0,0) 위치의 점
         System.out.println(zeroPoint.toString() + "입니다.");
-
         ColorPoint cp = new ColorPoint(10, 10);
-
         cp.setXY(5, 5);
         cp.setColor("RED");
         System.out.println(cp.toString() + "입니다.");
     }
 }
-
-class ColorPoint extends Point {
-    private String color;
-
-
-    public ColorPoint() {
-        super(0, 0);
-        this.color = "BLACK";
-    }
-
-    public ColorPoint(int x, int y) {
-        super(x, y);
-    }
-
-    public void setXY(int x, int y) {
-        move(x, y);
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String toString() {
-      //  return toString() ??????/
-    }
-
-}
-
 
 class Point {
     private int x, y;
@@ -61,4 +30,29 @@ class Point {
         this.y = y;
     }
 }
-*/
+
+class ColorPoint extends Point {
+    private String color;
+
+    public ColorPoint() {
+        super(0, 0);
+        this.color = "BLACK";
+    }
+
+    public ColorPoint(int x, int y) {
+        super(x, y);
+        this.color = "BLACK";
+    }
+
+    public void setXY(int x, int y) {
+        move(x, y);
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString() {
+        return color + "색의 (" + getX() + "," + getY() + ")의 점";
+    }
+}
