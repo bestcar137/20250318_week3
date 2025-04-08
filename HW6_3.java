e14ff519fadd1846ded35db04f22c801283b778e
bestcar137 <bestcar137@gmail.com>
2025-04-09 00:15:03 +0900

    WEEK6_HW solve with GPT

---
+++ HW6_3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class HW6_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("그만")) {
+                System.out.println("종료합니다...");
+                break;
+            }
+            String[] token = input.split(" ");
+            int num = token.length;
+            System.out.println("어절 개수는 " + num);
+        }
+    }
+}
