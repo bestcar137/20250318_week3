a9389e2058ae643ec88c82317c12da86cb1c2752
MainPC <bestcar137@gmail.com>
2025-04-19 18:08:42 +0900

    MainPC_firstTest

---
+++ HW7_1.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class HW7_1 {
+    public static void printBig(Vector<Integer> v) {
+        int max = 0;
+        for (int n : v) {
+            if (n > max) {
+                max = n;
+            }
+        }
+        System.out.print("정수(-1이 입력될 때까지)>> ");
+        System.out.println("가장 큰 수는 " + max);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Vector<Integer> v = new Vector<>();
+        while (true) {
+            int num = sc.nextInt();
+            if (num == -1) break;
+            if (num > 0) v.add(num);
+        }
+        printBig(v);
+    }
+}
+
---
+++ HW7_4.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+abstract class Shape {
+    public abstract void draw();
+}
+
+class Line extends Shape {
+    public void draw() {
+        System.out.println("Line");
+    }
+}
+
+class Rect extends Shape {
+    public void draw() {
+        System.out.println("Rect");
+    }
+}
+
+class circle extends Shape {
+    public void draw() {
+        System.out.println("Circle");
+    }
+}
+
+public class HW7_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Vector<Shape> v = new Vector<>();
+
+        while (true) {
+            System.out.print("삽입(1), 삭제(2), 모두 보기(3), 종료(4)>>");
+            int cmd = sc.nextInt();
+
+            switch (cmd) {
+                case 1:
+                    System.out.print("Line(1), Rect(2), Circle(3)>>");
+                    int type = sc.nextInt();
+                    switch (type) {
+                        case 1:
+                            v.add(new Line());
+                            break;
+                        case 2:
+                            v.add(new Rect());
+                            break;
+                        case 3:
+                            v.add(new circle());
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("삭제할 도형의 위치>>");
+                    int index = sc.nextInt();
+                    if (index < 0 || index >= v.size()) {
+                        System.out.println("삭제할 수 없습니다.");
+                    } else {
+                        v.remove(index);
+                    }
+                    break;
+
+                case 3:
+                    for (int i = 0; i < v.size(); i++) {
+                        v.get(i).draw();
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("프로그램종료");
+                    return;
+            }
+        }
+    }
+}
